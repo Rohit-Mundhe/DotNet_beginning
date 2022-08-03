56c059ab99453fe5c0754394d67189f926c03a54
Rohit-Mundhe <rohitmundhe14@gmail.com>
2022-08-03 10:38:14 +0530

    Add project files.

---
+++ YoutubeDotNet/Controllers/CategoryController.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using YoutubeDotNet.Data;
+using YoutubeDotNet.Models;
+
+namespace YoutubeDotNet.Controllers
+{
+    public class CategoryController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+        public CategoryController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
+        public IActionResult Index()
+        {
+            //var ObjectCategoryList = _db.categories.ToList();
+            //return View();
+            IEnumerable<Category> objCategoryList = _db.categories;
+            return View(objCategoryList);
+        }
+        //GET
+        public IActionResult Create()
+        {
+            return View();
+        }
+        //POST
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create()
+        {
+            return View();
+        }
+    }
+}
